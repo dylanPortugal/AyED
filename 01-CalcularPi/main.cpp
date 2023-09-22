8f4eb708d8bccacc39574d2a9dc20cf25f542e9f
dylanPortugal <145608283+dylanPortugal@users.noreply.github.com>
2023-09-22 14:59:59 -0300

    Actualizar main.cpp

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -3,26 +3,27 @@
 using namespace std; 
 
 int main() {
-    double pi_aproximado = 0.0;
+    double pi_ejercicio = 0.0;
+    double pi = 3.141592;
     int iteraciones = 0;
 
     while (true) {
         double termino = 4.0 / (2 * iteraciones+ 1);
 
         if (iteraciones % 2 == 0) {
-            pi_aproximado += termino;
+            pi_ejercicio += termino;
         } else {
-            pi_aproximado -= termino;
+            pi_ejercicio -= termino;
         }
         iteraciones++;
 
-        if (fabs(3.141592 - pi_aproximado) < 0.0000001) {
+        if (fabs(pi - pi_ejercicio) < 0.0000001) {
             break;
         }
     }
     
     cout.precision(6);
-    cout << "Valor aproximado de pi: " << fixed << pi_aproximado << endl;
+    cout << "Valor aproximado de pi: " << fixed << pi_ejercicio << endl;
 
     return 0;
 }
